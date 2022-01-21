22d7c3709361077f3d6de4215ab0d65553b7e014
fernando <lfernando1120@gmail.com>
2022-01-21 14:32:56 -0600

    iniaitl commit

---
+++ src/main/java/com/fernando/examplegke/ExampleGkeApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.examplegke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGkeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/examplegke/ExampleGkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.examplegke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
